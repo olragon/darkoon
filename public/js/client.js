e2106f6da932731e3bf8ea49dcf91e486d3026f1
Long Nguyen <olragon@gmail.com>
2013-08-31 17:11:50 +0700

    parse chrome form data form client

--- public/js/client.js
+++ public/js/client.js
@@ -53,6 +53,37 @@
 
       return false;
     });
+
+    // format post data
+    $form.find(':input[name="data"]').change(function () {
+      var val = $(this).val();
+      var parsed, formData;
+
+      if (val.match(/------/)) {
+        parsed = val.split(/------/);
+        formData = [];
+        if (parsed && parsed.length > 0) {
+          for (var _i=0; _i<parsed.length; _i++) {
+            parsed[_i] = parsed[_i].trim();
+            var matchName = parsed[_i].match(/(name=")([^"]+)/);
+            var matchValue = parsed[_i].match(/[^"]+$/);
+            var name, value = '';
+            if (matchValue) {
+              matchValue[0] = matchValue[0].trim();
+              value = matchValue[0] || '';
+            }
+            if (matchName) {
+              name = matchName[2];
+              formData.push(name + '=' + value);
+            }
+          }
+        }
+        $(this).val(formData.join('&'));
+      }
+
+      console.log(parsed);
+      console.log(formData.join('&'));
+    });
   });
 
 }(jQuery));
